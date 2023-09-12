0629f4159679fa97c1c7167e938d2a91ce45f667
Pablo <ezekielgonzalezr@gmail.com>
2023-09-11 20:45:44 -0600

    Create methods for categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+    def index 
+        @categories = current_user.categories
+    end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+        @transacts = @category.transacts
+    end
+end
