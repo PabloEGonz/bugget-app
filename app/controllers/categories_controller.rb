32e2e05dc47fd685ef0dc7a7950829678e57fb47
Pablo <ezekielgonzalezr@gmail.com>
2023-09-12 11:38:32 -0600

    Add new and create for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,25 @@
 class CategoriesController < ApplicationController
-    def index 
-        @categories = current_user.categories
-    end
+  def index
+    @categories = current_user.categories
+  end
 
-    def show
-        @category = Category.find_by(id: params[:id])
-        @transacts = @category.transacts
-    end
+  def show
+    @category = Category.find_by(id: params[:id])
+    @transacts = @category.transacts
+  end
+
+  def new
+    @new_category = Category.new
+  end
+
+  def create
+    category = Category.new(user: current_user)
+    category.assign_attributes
+  end
+
+  private
+
+  def categories_params
+    p
+  end
 end
