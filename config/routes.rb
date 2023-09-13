4fed1550c4508cc6ba80a5f46558c7ca378df729
Pablo <ezekielgonzalezr@gmail.com>
2023-09-12 20:11:36 -0600

    Set up nested  routes for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/user', to: 'users#index'
-  resources :categories
-  resources :transacts
+  resources :categories do 
+    resources :transacts, only: [:new, :create]
+  end
+  resources :transacts, only: [:show]
   root 'categories#index'
 end
