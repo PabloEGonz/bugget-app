355063e00c9aff140b64d698b949936ba432876c
Pablo <ezekielgonzalezr@gmail.com>
2023-09-14 07:22:01 -0600

    Improve splash screen styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def index
+        if current_user
+            redirect_to categories_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/user', to: 'users#index'
   get '/user_account', to: 'users#show'
   resources :categories do 
     resources :transacts, only: [:new, :create]
   end
   resources :transacts, only: [:show]
-  root 'categories#index'
+  root 'users#index'
 end
