d80abea0a580bfeb07f0b4ba1aa45755fe5fe33a
Pablo <ezekielgonzalezr@gmail.com>
2023-09-13 21:34:59 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/user', to: 'users#index'
+  get '/user_account', to: 'users#show'
   resources :categories do 
     resources :transacts, only: [:new, :create]
   end
