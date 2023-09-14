5fa429fe9325e4eb2aaa6805482894dce1bea3d6
Pablo <ezekielgonzalezr@gmail.com>
2023-09-14 14:21:00 -0600

    Disable flash notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-  def flash_alert_class(type)
-    case type
-    when 'error'
-      'alert-warning'
-    when 'success'
-      'alert-success'
-    else
-      'alert-secondary'
-    end
-  end
-
   def navbar_helper(path)
     if path.include?('transacts') || path.include?('categories/')
       'shared/back'
